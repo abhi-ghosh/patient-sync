28a2393ca1b21dff8264bd007095e3d3dfeb3943
Abhijit Ghosh <a7ghosh@gmail.com>
2026-07-31 16:41:22 +0530

    Add design system, navbar, and dark mode toggle

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, DM_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-inter",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmMono = DM_Mono({
   subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-dm-mono",
 });
 
 export const metadata = {
@@ -20,9 +22,11 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${inter.variable} ${dmMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col font-sans">
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,38 @@
+"use client";
+import { useState, useEffect } from "react";
+import {HeartPulse,Sun,Moon} from "lucide-react";
+
 export default function Home() {
+
+  const [darkMode, setDarkMode] = useState(false);
+  //Theme toggle function
+  const changeTheme = () => {
+    setDarkMode(prev => !prev);
+  }
+  //To make sure the theme is set on page load
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark",darkMode);
+  }, [darkMode]);
+
   return (
     <main>
-      <h1>Patient Sync</h1>
+      <nav className="bg-primary h-16 w-full py-2 px-6 lg:px-8 flex items-center justify-between">
+        <div className="flex items-center justify-center gap-4">
+          <div className="flex items-center justify-center bg-accent w-10 h-10 rounded-lg">
+            <HeartPulse className="text-primary-foreground"/>
+          </div>
+          <div>
+            <p className="text-primary-foreground font-bold">Patient Sync</p>
+            <p className="text-primary-foreground text-xs">Patient registration system</p>
+          </div>
+        </div>
+        <button onClick={changeTheme} className="bg-accent dark:bg-muted-foreground text-primary-foreground w-20 py-2 px-2
+          rounded-lg flex items-center justify-center gap-2 cursor-pointer
+          hover:scale-105 active:scale-95 transition-all duration-200 ease">
+            {darkMode ? <Sun className="w-4 h-4"/> : <Moon className="w-4 h-4"/>}
+            <p className="text-sm font-bold">{darkMode ? "Light" : "Dark"}</p>
+        </button>
+      </nav>
     </main>
   );
 }
